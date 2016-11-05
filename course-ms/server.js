503b9597d41667142d1f8d2b2b4699a585d32cb2
Josue Pirir <josuepirir@gmail.com>
2016-11-05 11:38:44 -0600

    ADD ABC microservice course, ADD postman collection by testing

---
+++ course-ms/server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('dotenv').config();
+
+const Confidence = require('confidence');
+const Glue = require('glue');
+
+const configFile = require('./config');
+const store = new Confidence.Store(configFile);
+const config = store.get('/', { env: process.env.NODE_ENV });
+
+const Manifest = config.server;
+const options = { relativeTo: __dirname };
+
+Glue.compose(Manifest, options, (err, server) => {
+    if (err){
+        throw err;
+    }
+
+    const basePlugins = [
+        {
+            register: require('./libs/db'),
+            options: { config: config.db}
+        }
+    ];
+
+    server.register(basePlugins, (err) => {
+        let basePath = config.context.basePath;
+        basePath = basePath && basePath !== '' ? '/' + basePath : '';
+
+        const routes = require('./routes').map((v, k) => {
+            v.path = basePath + v.path;
+            return v;
+        });
+
+        server.route(routes);
+
+        server.start((err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Server running at ' + server.info.uri);
+        });
+    });
+
+});
