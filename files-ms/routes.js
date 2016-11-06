be46cb88382f23e96e99df37b68828ac1eb95310
garfenter <garfenter@gmail.com>
2016-11-06 17:58:07 -0600

    Add first version of files microservice

---
+++ files-ms/routes.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Joi = require('joi');
+
+const Handlers = require('./handlers');
+
+module.exports = [
+    {
+        path: '/',
+        method: 'GET',
+        handler: Handlers.findAllFiles
+    },
+    {
+        path: '/{dbid}',
+        method: 'GET',
+        handler: Handlers.findFileByDbid,
+        config: {
+            validate: {
+                params: {
+                    dbid: Joi.number().integer().min(1)
+                }
+            }
+        }
+    },
+    {
+        path: '/{dbid}/download',
+        method: 'GET',
+        handler: Handlers.downloadFileByDbid,
+        config: {
+            validate: {
+                params: {
+                    dbid: Joi.number().integer().min(1)
+                }
+            }
+        }
+    },
+    {
+        path: '/',
+        method: 'POST',
+        handler: Handlers.uploadFile,
+        config: {
+            validate: {
+                payload: Handlers.Validator
+            }
+        }
+    },
+    {
+        path: '/{dbid}',
+        method: 'DELETE',
+        handler: Handlers.deleteFile,
+        config: {
+            validate: {
+                params: {
+                    dbid: Joi.number().integer().min(1)
+                }
+            }
+        }
+    }
+];
