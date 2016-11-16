145a390bd13173bf616e8e5d75ff2df836666a48
josuedani <josuepirir@kuak.me>
2016-11-15 22:41:52 -0600

    ADD Course ms
    First version by pull request

---
+++ course-ms/handlers.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const Joi = require('joi');
+
+exports.Validator = Joi.object().keys({
+    dbid: Joi.number().optional(),
+    institution: Joi.number().required(),
+    code: Joi.string().required(),
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    state: Joi.number().required()
+});
+
+// Find all Courses
+exports.findAllCourses = function (request, reply) {
+    request.app.db.query('SELECT * FROM COURSE', (err, rows, fields) => {
+        if (err) {
+            throw err;
+        }
+        reply(rows);
+    });
+}
+
+// Find Couse by dbid
+exports.findCourseByDbid = function (request, reply) {
+
+    const course = { dbid: request.params.dbid };
+
+    request.app.db.query('SELECT * FROM COURSE WHERE dbid = :dbid', course, (err, rows, fields) => {
+        if (err) {
+            throw err;
+        }
+        reply(rows);
+    });
+
+}
+
+// Create a new course
+exports.createCourse = function (request, reply) {
+    const course = {
+        institution: request.payload.institution,
+        code: request.payload.code,
+        name: request.payload.name,
+        description: request.payload.description,
+        state: request.payload.state
+    }
+
+    request.app.db.query('INSERT INTO COURSE (institution, code, name, description, state) VALUES (:institution, :code, :name, :description, :state)', course, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.insertId){
+            course.dbid = result.insertId;
+        }
+        reply(course);
+    });
+}
+
+// Update a existent course
+exports.updateCourse = function (request, reply) {
+    const course = {
+        dbid: request.payload.dbid,
+        institution: request.payload.institution,
+        code: request.payload.code,
+        name: request.payload.name,
+        description: request.payload.description,
+        state: request.payload.state
+    }
+
+    request.app.db.query('UPDATE  COURSE SET institution = :institution , code = :code, name = :name, description = :description, state = :state WHERE dbid = :dbid', course, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        reply(course);
+    });
+}
+
+// Delete a existent course
+exports.deleteCourse = function (request, reply) {
+    const course = {
+        dbid: request.params.dbid
+    };
+
+    request.app.db.query('DELETE FROM COURSE WHERE dbid = :dbid', course, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        reply(course);
+    });
+}
